1387603bf76b7eaa1140a236c7104f303040255f
Aigul Beck <aigul.beck@gmail.com>
2023-09-07 09:37:32 -0500

    Include built in methods practice

--- builtInMethods.js
+++ builtInMethods.js
@@ -7,16 +7,17 @@
 // No arguments are passed; toLowerCase() has one clear job which is to lowercase all letters that exist in the String
 // The return value is "hello world"
 "Hello World".toLowerCase();
-
+// This line of code changes the value of string to ALL lower case letter. Empty paranthesis program the print out to go through and return same given value.
 
 "Hello World".includes("Hello");
-
+// This method is being called on string "Hello World" and cheking if it includes "Hello". 
+// Since it does contain then `true` will be the return value.
 
 "Hello World".endsWith("Hello");
-
+// Above line of code is checking the "Hello World" string and will assign it to boolean false if string ends with "Hello"
 
 "Hello World".endsWith("rld");
-
+// This method is checking if string is equal to string value. That it will return true or false depending on input.
 
 
 // SECTION 2: Calling methods on variables assigned to strings.
@@ -24,6 +25,16 @@
 // Call a different built-in JavaScript method on each of your variables. 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
 // Include comments above each method call explaining the impact and return value of that method.
+
+// toLowerCAse method is calling varibale ingredient, that stores value "WATER" and printing out ALL Of the string in lower case
+var ingredient = "WATER".toLowerCase();
+console.log(ingredient);
+
+// Follwoing line of code declaring a variable named address, storing string "Washington" in it. 
+// Method endsWith is calling the string data and passing throught the boolean argument.
+// If argument passes as true then we ahould be able to print out value true.
+var address = "Washington".endsWith(("n"));
+console.log(address);
 
 // EXAMPLE
 // The startsWith() method is called on the firstName variable, which stores the string object "Jeff". 
@@ -40,3 +51,11 @@ console.log(firstName.startsWith("J"));
 // Call a different built-in JavaScript method on each of your variables. 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 // Include comments above each method call explaining the impact and return value of that method.
+
+// Line of code below calls the pop method on the months array and returns last string in the array.
+var months = ["April", "December", "March", "June"].pop();
+console.log(months);
+
+// Follwoing line of code declaring a variable named months2, calling the shift built in method on array and returning number of strings.
+var months2 = ["January", "July", "February", "May"].shift();
+console.log(months2);
